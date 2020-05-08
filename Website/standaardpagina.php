ed5ac9c4f85b317baa6152547e177c3c85fd933d
NXTTX <60346273+nxttx@users.noreply.github.com>
2020-05-08 12:32:53 +0200

    bug fixes+ standaard pagina

---
+++ Website/standaardpagina.php
@@ -0,0 +1,20 @@
+<?php
+$siteTitle= "standaarpagina"
+
+?>
+
+<?php include "includes/head.php" ?>
+<?php include "includes/header.php"?>
+<section>
+    <div class="container">
+        <br>
+        <h2 class="title is-3  has-text-centered">TITEL</h2>
+        <p class="subtitle is-6  has-text-centered">ONDERTITEL</p>
+        <div class="columns">
+
+        <?= $ads ?>
+
+        </div>
+    </div>
+</section>
+<?php include "includes/footer.html"?>
