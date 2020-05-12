9086ebeb5da16e3260c3181af7fb016f0974d377
rutgerbl <rutgerbles@gmail.com>
2020-05-12 11:10:52 +0200

    Eerste standaard opzet faq.php

---
+++ Website/faq.php
@@ -0,0 +1,23 @@
+<?php
+$siteTitle = "FAQ";
+?>
+
+<?php include "includes/head.php" ?>
+<?php include "includes/header.php" ?>
+    <section>
+        <div class="container">
+            <br>
+
+            <div class="card ">
+                <div class="card-content">
+                    <h2 class="title is-2  has-text-centered">Veel gestelde vragen</h2>
+                    <p class="subtitle is-5  has-text-centered">Hier vindt u de meest gestelde vragen en de antwoorden
+                        hierop. Staat uw vraag er niet tussen? Neem dan <a href="contact.php">contact </a>met ons op. </p>
+                </div>
+            </div>
+
+            <br>
+
+        </div>
+    </section>
+<?php include "includes/footer.html" ?>
