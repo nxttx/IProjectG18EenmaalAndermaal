5c56dfa70402204d33644a30de284db2df3f7938
rutgerbl <rutgerbles@gmail.com>
2020-05-12 13:16:47 +0200

    Updat faq.php Text added
    I think it's done!

--- Website/faq.php
+++ Website/faq.php
@@ -12,7 +12,44 @@ $siteTitle = "FAQ";
                 <div class="card-content">
                     <h2 class="title is-2  has-text-centered">Veel gestelde vragen</h2>
                     <p class="subtitle is-5  has-text-centered">Hier vindt u de meest gestelde vragen en de antwoorden
-                        hierop. Staat uw vraag er niet tussen? Neem dan <a href="contact.php">contact </a>met ons op. </p>
+                        hierop. Staat uw vraag er niet tussen? Neem dan <a href="contact.php">contact </a>met ons op.
+                    </p>
+
+                    <!--                    De vragen hier onder:-->
+
+                    <h2 class="title is-3 is-marginless">Kan ik mijn bod nog ongedaan maken als het als is
+                        verstuurd?</h2>
+                    <p>Nee, alle biedingen zijn definitief en kunnen dus niet meer ongedaan worden gemaakt. Wel kan u in
+                        overeenstemming met de verkoper uw bod intrekken, maar hier is de verkoper niet verplicht
+                        toe. </p>
+                    <br>
+
+                    <h2 class="title is-3 is-marginless">Kan ik mijn advertentie verwijderen als er al een bod is
+                        gedaan? </h2>
+                    <p>Nee alle verkopen en biedingen zijn definitief en kunnen dus niet meer ongedaan worden gemaakt.
+                        Dit kan alleen als er nog geen bod op de advertentie is gedaan of in overeenstemming met de
+                        koper, maar hier is de koper niet verplicht toe. </p>
+                    <br>
+
+                    <h2 class="title is-3 is-marginless">Hoe weet ik zeker of mijn data echt zo veilig is
+                        opgeslagen en behandeld als jullie zeggen?</h2>
+                    <p>Op dit moment is EenmaalAndermaal&trade; in de 'ALPHA' ontwikkelfase, als het uit de ontwikkelfase
+                        komt en een volledig werkende site is wordt de broncode 'Open Source'. Dit betekent dat u alle
+                        code kunt inzien en dus ook kan zien waar uw data naartoe gaat. Hiernaast kunt u altijd vragen
+                        stellen over uw dataverwerking, hier zullen wij (redelijkerwijs) altijd antwoord op geven.</p>
+                    <br>
+
+                    <h2 class="title is-3 is-marginless">Ik ben mij wachtwoord vergeten, wat doe ik nu? </h2>
+                    <p>Deze u kunt (binnenkort) aanpassen via een link onderaan de inlogpagina of via <a
+                                href="passwordForgot.php">deze </a>link.</p>
+                    <br>
+
+                    <h2 class="title is-3 is-marginless">Ik kan geen advertentie plaatsen, waardoor komt dit? </h2>
+                    <p>Dit kan op meerdere redenen hebben, controleer eerst uw internetverbinding en probeer het met een andere
+                        webbrowser. Doet het probleem zich nog steeds voor? Neem dan <a href="contact.php"> contact</a>
+                        op met een van onze medewerkers. Het kan voorkomen dat uw account (tijdelijk) is geblokkeerd
+                        omdat wij fraude of spam vermoeden. Is dit niet het geval, dan hoeft u zich geen zorgen te maken
+                        en wordt dit zo spoedig mogelijk opgelost.</p>
                 </div>
             </div>
 
