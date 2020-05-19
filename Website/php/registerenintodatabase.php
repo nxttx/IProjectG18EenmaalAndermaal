aaf99bf93fe6c4c9aa24bb31a516df20a731f41a
NXTTX <60346273+nxttx@users.noreply.github.com>
2020-05-19 09:21:42 +0200

    extra beveleging en documentatie

--- Website/php/registerenintodatabase.php
+++ Website/php/registerenintodatabase.php
@@ -1,9 +1,18 @@
 <?php include "dbh.php" ?>
 <?php
 $dbh = connectToDatabase();
+//gebruikersnaam check
 
+$sth = $dbh->prepare("SELECT COUNT(vraag) as hoeveelheid FROM gebruiker WHERE gebruikersnaam =:gebruikersnaam");
+$sth->bindParam(':gebruikersnaam', $gebruikersnaam);
+$gebruikersnaam = $_POST['gebruikersnaam'];
+$sth->execute();
+foreach ($sth->fetchAll(PDO::FETCH_ASSOC) as $row) {
+    $hoeveelheidGebruikersnamen = $row['hoeveelheid'];
+}
 
-if ($_POST['wachtwoordregel1'] == $_POST['wachtwoordregel2'] && strlen($_POST['wachtwoordregel1']) >= 8) {
+//first check
+if ($_POST['wachtwoordregel1'] == $_POST['wachtwoordregel2'] && strlen($_POST['wachtwoordregel1']) >= 8 && $hoeveelheidGebruikersnamen < 1) {
     $password = $_POST['wachtwoordregel1'];
     $finalPassword = password_hash($password, PASSWORD_DEFAULT);
 
@@ -23,16 +32,16 @@ VALUES( :gebruikersnaam, :voornaam, :achternaam, :adresregel1, :adresregel2, :po
     $sth->bindParam(':Vraag', $vraag);
     $sth->bindParam(':antwoordtekst', $antwoord);
 
-    $gebruikersnaam= $_POST['gebruikersnaam'];
-    $voornaam= $_POST['voornaam'];
-    $achternaam= $_POST['achternaam'];
-    $adresregel1= $_POST['adresregel1'];
-    $adresregel2=$_POST['adresregel2'];
-    $postcode= $_POST['postcode'];
-    $plaatsnaam= $_POST['plaatsnaam'];
-    $land=$_POST['land'];
-    $birthdate=$_POST['birthdate'];
-    $email=$_POST['email'];
+    $gebruikersnaam = $_POST['gebruikersnaam'];
+    $voornaam = $_POST['voornaam'];
+    $achternaam = $_POST['achternaam'];
+    $adresregel1 = $_POST['adresregel1'];
+    $adresregel2 = $_POST['adresregel2'];
+    $postcode = $_POST['postcode'];
+    $plaatsnaam = $_POST['plaatsnaam'];
+    $land = $_POST['land'];
+    $birthdate = $_POST['birthdate'];
+    $email = $_POST['email'];
     $finalPassword = $finalPassword;
     $vraag = $_POST['vraag'];
     $antwoord = $_POST['antwoord'];
@@ -41,22 +50,28 @@ VALUES( :gebruikersnaam, :voornaam, :achternaam, :adresregel1, :adresregel2, :po
 
     $title = "Registreren gelukt.";
     $subtitle = "";
-    $error_msg = "<p>Welkom ". $voornaam .", <br> Leuk dat je je bij ons aansluit! Samen gaan we opweg naar een duurzamere wereld.<br> Waarin hergebruik steeds van zelf sprekender wordt.<br> <br> Je moet nog even wachten tot een beheerder je aanvraag goed keurd. Maar tot die tijd kan je gewoon op de website rond kijken. <br> Groetjes, Het team van <a>EenmaalAndermaal</a></p>";
+    $error_msg = "<p>Welkom " . $voornaam . ", <br> Leuk dat je je bij ons aansluit! Samen gaan we opweg naar een duurzamere wereld.<br> Waarin hergebruik steeds van zelf sprekender wordt.<br> <br> Je moet nog even wachten tot een beheerder je aanvraag goed keurd. Maar tot die tijd kan je gewoon op de website rond kijken. <br> Groetjes, Het team van <a>EenmaalAndermaal</a></p>";
 
 } elseif ($_POST['wachtwoordregel1'] != $_POST['wachtwoordregel2']) {
     $title = "Foutmelding";
-       $subtitle = "";
+    $subtitle = "";
     $error_msg = '<div class="notification is-danger">
   Wachtwoorden zijn <b>niet</b> gelijk. 
 </div>';
 
-} elseif (strlen($_POST['wachtwoordregel1']) >= 8) {
+} elseif (strlen($_POST['wachtwoordregel1']) <= 8) {
     $title = "Foutmelding";
     $subtitle = "";
     $error_msg = '<div class="notification is-danger">
   Wachtwoord moet groter zijn dan 8 tekens. U heeft: ' . strlen($_POST['wachtwoordregel1']) . ' tekens.
 </div>';
 
+} elseif ($hoeveelheidGebruikersnamen > 0) {
+    $title = "Foutmelding";
+    $subtitle = "";
+    $error_msg = '<div class="notification is-danger">
+  Gebruikersnaam bestaald al. U had <b>\'' . $_POST['gebruikersnaam'] . '\'</b>.
+</div>';
 } else {
     $title = "Foutmelding";
     $subtitle = "";
@@ -91,5 +106,6 @@ $siteTitle = "registreren part 2";
     </section>
 <?php include "../includes/footer.html" ?>
 <?php
-//"delete from gebruiker where gebruikersnaam = 'NXTTX'"
+//"delete from gebruiker where gebruikersnaam = 'iets'"
+//"select * from gebruiker"
 ?>
