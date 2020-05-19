a457e6d4afb810bdcf908d3b84f7d61001ed5275
NXTTX <60346273+nxttx@users.noreply.github.com>
2020-05-19 14:27:47 +0200

    basics done

--- Website/product.php
+++ Website/product.php
@@ -6,14 +6,12 @@ $productNummer = $_GET['pn'];
 $productpage = "";
 
 // get info for the page
-$sth = $dbh->prepare('SELECT V.titel, V.beschrijving, V.startprijs, V.Betalingswijze, V.betalingsinstructie, V.plaatsnaam, V.land, 
-       V.LooptijdbeginDag, V.LooptijdbeginTijdstip, V.Verzendkosten, V.verkoper, V.VeilinGesloten,V.Verkoopprijs, 
-       V.views, B.filenaam 
-FROM Voorwerp V 
+$sth = $dbh->prepare('SELECT V.titel, V.beschrijving, V.startprijs, V.Betalingswijze, V.betalingsinstructie, V.plaatsnaam, V.land,
+       V.LooptijdbeginDag, V.LooptijdbeginTijdstip, V.Verzendkosten, V.verkoper, V.VeilinGesloten,V.Verkoopprijs,
+       V.views, B.filenaam
+FROM Voorwerp V
 	JOIN bestand B on V.voorwerpnummer = B.voorwerp
-	WHERE voorwerpnummer = :productnummer ');
-
-
+ 	WHERE voorwerpnummer = :productnummer');
 $sth->bindParam(':productnummer', $pn);
 $pn = $productNummer;
 $sth->execute();
@@ -22,28 +20,58 @@ $sth->execute();
 foreach ($sth->fetchAll(PDO::FETCH_ASSOC) as $row) {
     $siteTitle = $row['titel'];
     $productpage = '
-        <h2 class="title is-2  has-text-centered">' . $row['titel'] . '</h2>
+
+        <h2 class="title is-1  has-text-centered">' . $row['titel'] . '</h2>
+        <br>
         <div class="columns">
                 <div class="column is-half">
                     <figure class=\"image objectfit-cover\">
-                        <img src=" ' .$row['filenaam'] .'" alt="img">
+                        <img src=" ' . $row['filenaam'] . '" alt="img">
                     </figure>
                 </div>
                 <div class="column is-half">
-                        <h3 class="title is-8"> &euro;' . $row['startprijs']  . '</h3>
+                        <h3 class="title is-8"> &euro;' . $row['startprijs'] . '</h3>
                         <p>
-                        
-                        
+                           ' . $row['beschrijving'] . '
+                        </p>
+                        <br>
+                        <form action="" method="">
+
+                            <label for="bod" class="label">Uw bod: *</label>
+                            <label class="checkbox">
+                                <input type="checkbox" required>
+                                Ik ga akoord met <a href="tos.php" target="_blank"> de gebruikersvoorwaarden</a>
+                            </label>
+                            <div class="field has-addons">
+
+                                <div class="control">
+                                    <input class="input is-primary" type="text" name="bod"
+                                           id="bod" value="&euro;" maxlength="50" minlength="5" required>
+                                </div>
+                                <input class="button is-primary" type="submit" value="breng bod uit">
+
+                            </div>
+
+                        </form>
+                        <br>
+                        <p>
+                        <b>Verkoper:</b> '. $row['verkoper'].'
+                        <br>
+                        <b>locatie verkoper:</b> '.$row['land'].$row['plaatsnaam'].'
+                        <br><br>
+                        <b>Betalingswijze:</b> '.$row['Betalingswijze'].'
+                        <br>
+                        <b>Betalingsinstructie:</b> '.$row['betalingsinstructie'].'
                         </p>
                 </div>
         </div>
-        
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+
+
     ';
 }
 
