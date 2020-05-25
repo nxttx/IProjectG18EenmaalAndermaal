c8e40841bdf89e9d6561e7bb9b2d68929b711ac9
rutgerbl <rutgerbles@gmail.com>
2020-05-25 12:25:26 +0200

    Local commit / for favicon

--- Website/includes/head.php
+++ Website/includes/head.php
@@ -6,6 +6,6 @@
     <link rel="stylesheet" href="../css/mystyles.css">
     <link href="https://kit-free.fontawesome.com/releases/latest/css/free.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/customstyle.css">
-    <link rel="icon" href="../Data/icon/favicon.ico">
+    <link rel="icon" href="favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
