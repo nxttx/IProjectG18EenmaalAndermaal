2a3a1db443157886e0e5ab87a4c61f6e289d3279
rutgerbl <rutgerbles@gmail.com>
2020-05-27 13:01:30 +0200

    Feature / deleteUSER

--- Website/contact.php
+++ Website/contact.php
@@ -16,7 +16,7 @@ $siteTitle = "Contact";
                     met een van onze medewerkers?</p>
 
 
-                <p class="subtitle is-5  has-text-centered">Stuur een mail naar <a href="mailto:Sander.Leer@han.nl">ons
+                <p class="subtitle is-5  has-text-centered">Stuur een mail naar <a href="mailto:iproject18@protonmail.com">ons
                         team.</a></p>
 
                 <p class="subtitle is-6 has-text-centered is-marginless">Of kom gezellig langs.</p>
--- Website/gegevensVerwijderen.php
+++ Website/gegevensVerwijderen.php
@@ -7,113 +7,160 @@ $dbh = connectToDatabase();
 
 <?php include "includes/head.php" ?>
 <?php include "includes/header.php" ?>
-    <section>
-        <div class="container">
-            <br>
-            <div class="card ">
-                <div class="card-content">
+<section>
+    <div class="container">
+        <br>
+        <div class="card ">
+            <div class="card-content">
 
 
-                    <?php if (!isset($_SESSION['user'])) { ?>
-                        <h2 class="title is-3">U bent nog niet ingelogd, u wordt doorgestuurd naar de inlogpagina</h2>
-                        <h3 class="subtitle is-5">Gebeurt dit niet automatisch binnnen enkele seconden? Klik dan <a
-                                    href="login.php">hier.</a></h3>
-                        <script>
-                            setTimeout(function () {
-                                window.location.href = 'login.php';
-                            }, 2000)
-                        </script>
+                <?php if (!isset($_SESSION['user'])) { ?>
+                    <h2 class="title is-3">U bent nog niet ingelogd, u wordt doorgestuurd naar de inlogpagina</h2>
+                    <h3 class="subtitle is-5">Gebeurt dit niet automatisch binnnen enkele seconden? Klik dan <a
+                                href="login.php">hier.</a></h3>
+                    <script>
+                        setTimeout(function () {
+                            window.location.href = 'login.php';
+                        }, 2000)
+                    </script>
 
 
-                    <?php ;
-                    } elseif (isset($_SESSION['user'])) {
-                    //Dit wordt alle PHP voor _SESSION['user'];
-                    $username = $_SESSION['user'];
-                    $deleteUser = '
+                <?php ;
+                } elseif (isset($_SESSION['user'])) {
+                //Dit wordt alle PHP voor _SESSION['user'];
+                $title = "";
+                $subtitle = "";
+                $error_msg = "";
+
+                $username = $_SESSION['user'];
+                $deleteUser = '
 
 <form method="post" action="gegevensVerwijderen.php" class="has-text-centered">
-    
+    <div class="columns">
+    <div class="column"></div>
     <div class="column is-half ">
         <label for="gebruikersnaam" class="label">Uw gegevens worden verwijderd voor het account met gebruikersnaam:</label>
-        <input class="input is-danger is-halfsize " type="text" name="gebruikersnaam"
+        <input class="input is-warning is-halfsize " type="text" name="gebruikersnaam"
             id="gebruikersnaam" value="' . $username . '" disabled="disabled">
-    </div><br><br><br>
+    </div>
+    <div class="column"></div>
+    </div>
                     
                     <!--Wachtwoord hier invullen -->
     <div class="column field">
          <label for="wachtwoord" class="label">Vul uw wachtwoord in om uw identiteit te bevestigen</label>
          <div class="control has-icons-left">
-            <input class="input is-danger" type="password" name="wachtwoord"
+            <input class="input is-warning" type="password" name="wachtwoord"
                id="wachtwoord" placeholder="********" required>
             <span class="icon is-small is-left">
             <i class="fas fa-key"></i>
             </span>
          </div>
          
-         
+         <br>
+          <label class="checkbox">
+              <input type="checkbox" required>
+                Ik weet zeker dat ik mijn account wil verwijderen.
+          </label>
          <br>
          <br>
          <div class="field has-text-centered">
-            <input type="submit" class="button is-danger" value="Klik hier om uw gegevens te verwijderen" >
-        
+            <input type="submit" class="button is-danger" value="Klik hier om uw gegevens te verwijderen">
+                
         </div>
         </form>
                             
                         ';
 
 
-                    if ($_SERVER['REQUEST_METHOD'] == '_POST') {
-//wachtwoord check
-                        $password = $_POST['wachtwoordregel'];
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-                        $qry = $dbh->prepare("SELECT wachtwoord FROM gebruiker WHERE gebruikersnaam=:username");
-                        $qry->execute(array($username));
-                        $hash = $qry->fetch();
+                    //wachtwoord check
 
-                        if (empty($hash)) {
-                            http_response_code(404);
-                            echo 'hallo ja iets 404';
+                    $username = $_SESSION['user'];
+                    $password = $_POST['wachtwoord'];
+
+                    $qry = $dbh->prepare("SELECT wachtwoord FROM gebruiker WHERE gebruikersnaam=:username");
+
+                    $qry->execute(array($username));
+
+                    $hash = $qry->fetch();
+
+                    if (empty($hash)) {
+                        http_response_code(404);
+                    } else {
+                        $pw = $hash[0];
+
+                        if (password_verify($password, $pw)) {
+
+                            $sth = $dbh->prepare('DELETE FROM gebruikerstelefoon WHERE gebruiker=:gebruiker');
+                            $sth->bindParam(':gebruiker', $gebruiker);
+                            $gebruiker = $username;
+                            $sth->execute();
+                            $sth = $dbh->prepare('UPDATE voorwerp SET verkoper = \'VERWIJDERDE_GEBRUIKER\' WHERE verkoper=:gebruiker');
+                            $sth->bindParam(':gebruiker', $gebruiker);
+                            $gebruiker = $username;
+                            $sth->execute();
+                            $sth = $dbh->prepare('UPDATE voorwerp SET koper = \'VERWIJDERDE_GEBRUIKER\' WHERE koper=:gebruiker');
+                            $sth->bindParam(':gebruiker', $gebruiker);
+                            $gebruiker = $username;
+                            $sth->execute();
+                            $sth = $dbh->prepare('UPDATE bod SET gebruiker = \'VERWIJDERDE_GEBRUIKER\' WHERE gebruiker=:gebruiker');
+                            $sth->bindParam(':gebruiker', $gebruiker);
+                            $gebruiker = $username;
+                            $sth->execute();
+                            $sth = $dbh->prepare('DELETE FROM verkoper WHERE gebruiker=:gebruiker');
+                            $sth->bindParam(':gebruiker', $gebruiker);
+                            $gebruiker = $username;
+                            $sth->execute();
+                            $sth = $dbh->prepare('DELETE FROM gebruiker WHERE gebruikersnaam=:gebruiker');
+                            $sth->bindParam(':gebruiker', $gebruiker);
+                            $gebruiker = $username;
+                            $sth->execute();
+
+                            $deleteUser = '';
+                            $subtitle = '<br><p class="subtitle has-text-centered is-marginless">U kunt nu gerust de pagina sluiten of naar een andere pagina gaan.</p>';
+                            $error_msg = "";
+                            session_destroy();
+
                         } else {
-                            $pw = $hash[0];
-
-                            if (password_verify($password, $pw)) {
-                                echo 'wachtwoord klopt mooiman';
-                                $deleteUser='';
-                                //de statements om ingelogde gebruiker stapsgewijs te verwijderen
-                                $sth = $dbh->prepare('DELETE FROM gebruikerstelefoon WHERE gebruiker=:gebruiker');
-                                $sth = $dbh->prepare('UPDATE voorwerp SET verkoper = \'VERWIJDERDE_GEBRUIKER\' WHERE verkoper=:gebruiker');
-                                $sth = $dbh->prepare('UPDATE voorwerp SET koper = \'VERWIJDERDE_GEBRUIKER\' WHERE koper=:gebruiker');
-                                $sth = $dbh->prepare('UPDATE bod SET gebruiker = \'VERWIJDERDE_GEBRUIKER\' WHERE gebruiker=:gebruiker');
-                                $sth = $dbh->prepare('DELETE FROM verkoper WHERE gebruiker=:gebruiker');
-                                $sth = $dbh->prepare('DELETE FROM gebruiker WHERE gebruikersnaam=:gebruiker');
-
-                                $sth->bindParam(':gebruiker', $gebruiker);
-                                $gebruiker = $username;
-                                $sth->execute();
-                            }
-                            else{
-                                echo 'wachtwoord klopt NIET jammerman';
-                            }
+                            $subtitle = "";
+                            $error_msg = '<br><div class="notification is-danger has-text-centered">
+  Uw wachtwoord klopt niet!
+  <br>
+</div>
+';
                         }
                     }
 
-                    ?>
-                        <!--Weergeven op de gegevensVerwijderen pagina als ingelogd is-->
-                        <h1 class="title is-1 has-text-centered">Uw account verwijderen</h1>
-                        <h3 class="subtitle is-size-5 has-text-centered">Uw account en al uw opgeslagen gegevens worden
-                            direct verwijderd. <br>U kunt uw gegevens dus niet later terughalen!</h3>
-
-                        <?=var_dump($username);?>
-                    <?=$deleteUser?>
-
-                    <?php }; ?>
+                    $dbh = null;
 
 
-                </div>
+                }
+
+                ?>
+                    <!--Weergeven op de gegevensVerwijderen pagina als ingelogd is-->
+                    <h1 class="title is-1 has-text-centered">Uw account verwijderen</h1>
+                    <h3 class="subtitle is-size-5 has-text-centered">Uw account en al uw opgeslagen gegevens zijn
+                        direct verwijderd.</h3>
+                    <?= $deleteUser ?>
+                    <?= $title ?>
+                    <?= $subtitle ?>
+                    <?= $error_msg ?>
+
+
+                <?php }; ?>
+
+
             </div>
-
         </div>
-    </section>
-    <br>
+
+    </div>
+</section>
+<br>
 
 <?php include "includes/footer.php" ?>
+<script>
+    <
+    alert('LET OP! Al uw gegevens worden verwijderd!')
+</script>
