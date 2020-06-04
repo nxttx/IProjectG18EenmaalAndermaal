0524310ef6b8d462ed5cbba3fd1065d191563614
NXTTX <60346273+nxttx@users.noreply.github.com>
2020-06-04 19:50:07 +0200

    fixed categorieën boom en footer

--- Website/categorieen.php
+++ Website/categorieen.php
@@ -38,14 +38,14 @@ for ($a = 0; $a < $countH1; $a++) {
     $rubriekenLijstDesktop .= "<br><h2 class='link title is-8 is-marginless'>" . $header1[$a] . "</h2>";
     $sql = "SELECT (rubrieknaam) as 'h2 naam', (rubrieknummer) as nummer FROM rubriek WHERE Rubriek = $h1Rubrieknummer[$a];";
     foreach ($dbh->query($sql) as $row) {
-        $rubriekenLijstDesktop .= '<p class="link"><a href="#top" onclick="showSubSub( ' . $row['nummer'] . ')">' . $row['h2 naam'] . $a. '</a></p>';
+        $rubriekenLijstDesktop .= '<p class="link"><a href="#top" onclick="showSubSub( ' . $row['nummer'] . ')">' . $row['h2 naam'] . '</a></p>';
     }
 }
 
 //mobile:
 for ($a = 0; $a < $countH1; $a++) {
     $rubriekenLijstMobile .= "<br><h2 class='link title is-8 is-marginless'>" . $header1[$a] . "</h2>";
-    $sql = "SELECT (rubrieknaam) as 'h2 naam', (rubrieknummer) as nummer FROM rubriek WHERE Rubriek = $a+1;";
+    $sql = "SELECT (rubrieknaam) as 'h2 naam', (rubrieknummer) as nummer FROM rubriek WHERE Rubriek = $h1Rubrieknummer[$a];";
     foreach ($dbh->query($sql) as $row) {
         $rubriekenLijstMobile .= '<p class="link"><a href="categorieensubsub.php?rk=' . $row['nummer'] . '">' . $row['h2 naam'] . '</a></p>';
     }
